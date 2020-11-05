b9add0ffe7e5f5f348497785204934ccac8904c6
Thrynn Jonella <thjonell@si-p4.kzn.21-school.ru>
2020-11-05 18:41:36 +0300

    done

---
+++ ft_lstclear.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thjonell <thjonell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/02 21:38:09 by thjonell          #+#    #+#             */
+/*   Updated: 2020/11/04 23:12:53 by thjonell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	if (!lst)
+		return ;
+	while (*lst)
+	{
+		ft_lstdelone(*lst, del);
+		*lst = (*lst)->next;
+	}
+	*lst = NULL;
+}
